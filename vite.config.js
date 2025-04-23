9df337c2d96a373b93c9cfb5ee582acaa3836c5e
israel.moura <israel.moura@sankhya.com.br>
2025-04-23 14:08:57 -0300

    initial commit2

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,30 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 import { resolve } from 'path';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [
-      react(),
-      VitePWA({
-        strategies: 'injectManifest',
-        injectManifest: {
-          swSrc: resolve(__dirname, 'src/custom-sw.ts'),
-          swDest: 'service-worker.js',
-        },
-        registerType: 'autoUpdate',
-        devOptions: {
-          enabled: false,
-        },
-        workbox: {
-          skipWaiting: true,
-          clientsClaim: true,
-        }
-      })
-    ],
-    build: {
-      outDir: 'dist',
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, 'index.html'), // ✅ apenas o index
-        }
-      }
-    },
-    publicDir: 'public',
-    resolve: {
-      alias: {
-        '@': resolve(__dirname, 'src'),
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      strategies: 'injectManifest',
+      injectManifest: {
+        swSrc: resolve(__dirname, 'src/custom-sw.ts'),
+        swDest: 'service-worker.js'
       },
-    },
-  };
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: false
+      },
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src') // ✅ Isso resolve os imports com @/
+    }
+  }
 });
